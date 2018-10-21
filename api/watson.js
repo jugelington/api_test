f8970e5d58982a333b5e1b3a4d7e982c4a18d219
jugelington <julianbeightondykes@gmail.com>
2018-10-21 16:06:58 +0100

    maybe got config working

--- api/watson.js
+++ api/watson.js
@@ -1,6 +1,6 @@
 const PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
 
-const { PI_USERNAME, PI_PASSWORD, PI_URL } = require('../config/');
+// const { PI_USERNAME, PI_PASSWORD, PI_URL } = require('../config/');
 
 // const PI_USERNAME = process.env.PI_USERNAME || require('../config');
 // const PI_PASSWORD = process.env.PI_PASSWORD || require('../config');
@@ -8,9 +8,9 @@ const { PI_USERNAME, PI_PASSWORD, PI_URL } = require('../config/');
 
 const personalityInsights = new PersonalityInsightsV3({
   version: '2017-10-13',
-  username: process.env.PI_USERNAME || PI_USERNAME,
-  password: process.env.PI_PASSWORD || PI_PASSWORD,
-  url: process.env.PI_URL || PI_URL
+  username: process.env.PI_USERNAME,
+  password: process.env.PI_PASSWORD,
+  url: process.env.PI_URL
 });
 
 module.exports = personalityInsights;
